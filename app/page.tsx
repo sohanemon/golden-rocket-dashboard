ad1389b88229abe492cea6c6e02a8aea80c682b9
sohanemon <m.sohanemon@gmail.com>
2023-07-02 16:33:14 +0600

    feat: card

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Actions from './actions';
 import Hero from './hero';
+import Statistics from './statistics';
 
 export default function Home() {
   return (
     <main className='p-10 grow'>
       <Actions />
       <Hero />
+      <Statistics />
     </main>
   );
 }
---
+++ app/statistics.tsx
@@ -0,0 +1,56 @@
+import Image from 'next/image';
+
+export default function Statistics() {
+  return (
+    <section className='grid grid-cols-2 gap-6 mt-6'>
+      {data.map((_) => (
+        <div className='p-5 ring-1 ring-theme-border rounded-xl' key={_.title}>
+          <p className='text-sm text-white/60'>Statistics</p>
+          <p className='my-1 text-sm font-semibold'>{_.title}</p>
+          <p className='my-4 text-4xl font-semibold'>{_.amount}</p>
+          {_.increasing !== undefined && (
+            <button
+              className={`p-2.5 rounded-lg  flex items-center gap-2 text-sm ${
+                _.increasing
+                  ? 'text-theme-teal bg-theme-teal/10'
+                  : 'text-theme-pink bg-theme-pink/10'
+              }`}
+            >
+              {_.trendValue}
+              <Image
+                src={`/assets/${_.increasing ? 'up' : 'down'}.svg`}
+                alt='icon'
+                width={14}
+                height={10}
+              />
+            </button>
+          )}
+        </div>
+      ))}
+    </section>
+  );
+}
+const data = [
+  {
+    title: 'TOTAL VALUE BURNT',
+    amount: '$ 342.954,56',
+    trendValue: '+21.01%',
+    increasing: true,
+  },
+  {
+    title: 'TOTAL CIRCULATING SUPPLY',
+    amount: '342.954,56',
+    trendValue: '+21.01%',
+    increasing: false,
+  },
+  {
+    title: 'PERCENTAGE BURNT',
+    amount: '69%',
+  },
+  {
+    title: 'CURRENT MARKET CAP',
+    amount: '$ 342.954,56',
+    trendValue: '+21.01%',
+    increasing: true,
+  },
+];
