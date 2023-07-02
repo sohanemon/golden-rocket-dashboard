d2291ab9869a765d34039d32292fe29945ab7c49
sohanemon <m.sohanemon@gmail.com>
2023-07-02 21:15:05 +0600

    fix: btn

--- app/actions.tsx
+++ app/actions.tsx
@@ -1,11 +1,11 @@
 export default function Actions() {
   return (
-    <section className='flex justify-end gap-6 pb-10'>
-      <button className='py-3 font-semibold uppercase rounded-lg px-9 gradient '>
+    <section className='flex justify-end gap-6 pb-10 max-sm:text-xs whitespace-nowrap '>
+      <button className='lg:py-3 py-2.5 font-semibold uppercase rounded-lg px-4 lg:px-9 gradient '>
         buy $rocket
       </button>
       <button className='p-0.5 font-semibold uppercase rounded-lg gradient'>
-        <div className='px-[35px] py-[11px] rounded-lg bg-theme-black'>
+        <div className='lg:px-[35px] px-3 py-2.5 lg:py-[11px] rounded-lg bg-theme-black'>
           connect wallet
         </div>
       </button>
