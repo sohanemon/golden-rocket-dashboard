2daaf88758cd75a72083ce4e69748fff061a3aee
sohanemon <m.sohanemon@gmail.com>
2023-07-02 14:50:53 +0600

    init: colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,15 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        theme: {
+          pink: '#c14552',
+          yellow: '#fbc218',
+          teal: '#63D2A1',
+          black: '#0b0b0b',
+        },
       },
     },
   },
   plugins: [],
-}
+};
